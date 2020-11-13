6cf2365f7e0fa0c19bdee2f519eb1e0a1e38bf51
nnwijesuriya <51411336+nnwijesuriya@users.noreply.github.com>
2020-11-14 04:17:46 +0530

    update

--- Application/HRIS/src/app/managers/tabs1/notices/notices.page.ts
+++ Application/HRIS/src/app/managers/tabs1/notices/notices.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { notice, noticesService } from './notices.service';
 
 @Component({
   selector: 'app-notices',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticesPage implements OnInit {
 
-  constructor() { }
+  constructor(private noticeService: noticesService, private router : Router, private toastCtrl: ToastController) { }
+
+  notice : notice ={
+    heading: '',
+    description: '',
+    priority: ''
+  };
 
   ngOnInit() {
+  }
+
+  async succesToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Your notice has been added',
+      duration: 2000  
+    });
+    toast.present();
+  }
+
+    async failToast() {
+    const toast =  await this.toastCtrl.create({
+      message: 'There was a problem adding your notice',
+      duration: 2000
+    });
+    toast.present();  
+  } 
+
+  submit()
+  {
+    this.noticeService.addnotice(this.notice, this.notice.priority).then(() => {
+      this.router.navigateByUrl('tabs/notices');
+      this.succesToast();
+    }, err => {
+      this.failToast();
+    });
+    console.log(this.notice);
+  }
+
+  cancel()
+  {
+   this.notice.heading="";
+   this.notice.description=""
+   this.notice.priority=""
   }
 
 }
---
+++ Application/HRIS/src/app/managers/tabs1/notices/notices.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+ 
+export interface notice {
+  id?: string,
+  heading: string,
+  description: string;
+  priority: string;
+}
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class noticesService {
+
+  private noticesU: Observable<notice[]>;
+  private noticeCollectionU: AngularFirestoreCollection<notice>;
+
+  private noticesH: Observable<notice[]>;
+  private noticeCollectionH: AngularFirestoreCollection<notice>;
+
+  private noticesN: Observable<notice[]>;
+  private noticeCollectionN: AngularFirestoreCollection<notice>;
+
+  constructor(private afs: AngularFirestore) {
+
+    this.noticeCollectionU = this.afs.collection<notice>('notices/priority/high priority');
+    this.noticesU = this.noticeCollectionU.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+
+    this.noticeCollectionH = this.afs.collection<notice>('notices/priority/important');
+    this.noticesH = this.noticeCollectionH.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+
+    this.noticeCollectionN = this.afs.collection<notice>('notices/priority/formal notice');
+    this.noticesN = this.noticeCollectionN.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
+
+  addnotice(notices: notice, prio: string): Promise<DocumentReference> {
+    if(prio=="Urgent Notice"){
+      return this.noticeCollectionU.add(notices);
+    } else if(prio=="Important Notice"){
+      return this.noticeCollectionH.add(notices);
+    }else
+    {
+      return this.noticeCollectionN.add(notices);
+    }
+  }
+}
